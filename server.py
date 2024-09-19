00b14693dade620842d5172798bab896dedbc584
ajmiller611 <ajmiller611@live.com>
2024-09-19 12:56:35 -0400

    updated after pylint check

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+    Emotion prediction applicaion using Flask
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,25 +8,32 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    '''
+        Return data from the Watson NLP Library for emotion prediction.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        anger_score = response['anger']
-        disgust_score = response['disgust']
-        fear_score = response['fear']
-        joy_score = response['joy']
-        sadness_score = response['sadness']
 
-        return (f"For the given statement, the system response is 'anger': {anger_score}, "
-                f"'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, "
-                f"and 'sadness': {sadness_score}. The dominant emotion is {response['dominant_emotion']}.")
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
+
+    return (f"For the given statement, the system response is 'anger': {anger_score}, "
+            f"'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, "
+            f"and 'sadness': {sadness_score}. "
+            f" The dominant emotion is {response['dominant_emotion']}.")
 
 @app.route("/")
 def render_index_page():
+    '''
+        Return home page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
